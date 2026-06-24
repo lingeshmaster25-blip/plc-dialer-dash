85d8525a9854a5a039d352b66b3c978f419f67f1
lingeshmaster25-blip <lingeshmaster25@gmail.com>
2026-06-24 16:52:31 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -126,7 +126,7 @@ function HmiDashboard() {
       >
         {/* Logo */}
         <div style={{ display: "flex", alignItems: "center" }}>
-          <img src="/trilo-logo.png" alt="Trilo Automation" style={{ height: 54, objectFit: "contain" }} />
+          <img src="/trilo-logo.png" alt="Trilo Automation" style={{ height: 70, objectFit: "contain" }} />
         </div>
 
         {/* Status pill */}
