2fa385d54724ec318826efb123a15749cbbb887c
lingeshmaster25-blip <lingeshmaster25@gmail.com>
2026-07-22 11:07:38 +0530

    Update maintenance.tsx

--- src/routes/maintenance.tsx
+++ src/routes/maintenance.tsx
@@ -1,4 +1,4 @@
-=import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { useState, useEffect } from "react";
 import { DashboardShell } from "@/components/DashboardShell";
 import { hmiApi } from "@/lib/hmi-api";
